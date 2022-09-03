745f21931bca3348fa1183ada8c3e84bf6beeea6
VoVanToan26 <99601581+VoVanToan26@users.noreply.github.com>
2022-09-03 23:58:16 +0700

    ok header 4

--- src/components/Poper/Menu/MenuItems.js
+++ src/components/Poper/Menu/MenuItems.js
@@ -1,7 +1,13 @@
+import Button from "~/components/Button";
+
+import classNames from 'classnames/bind';
+import styles from './Menu.module.scss';
+const cx = classNames.bind(styles);
+
 function MenuItem({data}) {
     console.log(data)
     return ( 
-        <parseInt>{JSON.stringify(data)}</parseInt>
+        <Button className={cx('menu-item')} leftIcon={data.icon}  to={data.to}>{data.title}</Button>
      );
 }
 
--- src/components/Poper/Menu/index.js
+++ src/components/Poper/Menu/index.js
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind';
-import Tippy from '@tippyjs/react';
+import Tippy from '@tippyjs/react/headless';
 import { Wrapper as PopperWrapper } from '~/components/Poper';
 import MenuItem from './MenuItems';
 import styles from './Menu.module.scss';
@@ -7,18 +7,16 @@ const cx = classNames.bind(styles);
 
 function Menu({ children, items = [] }) {
     const renderItems = () => {
-        return items.map((item, index) => 
-        <MenuItem key={index} data={item} />);
+        return items.map((item, index) => <MenuItem key={index} data={item} />);
     };
     return (
         <Tippy
             interactive
+            delay={[0, 700]}
             placement="bottom-end"
             render={(attrs) => (
-                <div className={cx('content')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper>
-                        <h2>{renderItems()}</h2>
-                    </PopperWrapper>
+                <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+                    <PopperWrapper className={cx('menu-poper')}>{renderItems()}</PopperWrapper>
                 </div>
             )}
         >
